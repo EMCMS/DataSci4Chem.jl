a22e0d58a75cf048f8310ee5b855ffbb3ee181cd
Saer Samanipour <s.samanipour@uva.nl>
2023-05-29 14:32:53 +0200

    Adding initial packages

--- src/DataSci4Chem.jl
+++ src/DataSci4Chem.jl
@@ -1,5 +1,24 @@
 module DataSci4Chem
 
+using CSV
+using DataFrames 
+using Plots
+import StatsPlots as sp 
+using Statistics
+using LinearAlgebra
+using LsqFit
+import Pluto
+using IJulia
+
+
+export notebook
+
+
 # Write your package code here.
+
+# 1+1
+# Plouto.run()
+
+
 
 end
