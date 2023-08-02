0b36f8d17a03b85673be6183d949afafd219a80d
Documenter.jl <documenter@juliadocs.github.io>
2023-08-02 16:06:29 +0000

    build based on e9d3327

--- dev/Test_deploy.jl
+++
@@ -1,6 +0,0 @@
-using LiveServer
-using Documenter
-
-include("/Users/saersamanipour/Desktop/dev/pkgs/DataSci4Chem.jl/docs/make.jl") 
-cd("/Users/saersamanipour/Desktop/dev/pkgs/DataSci4Chem.jl/docs/build/")
-serve()
