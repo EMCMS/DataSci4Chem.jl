b8f41af559c6cfce5c99b3c6e7d6e4631e53159c
Saer Samanipour <s.samanipour@uva.nl>
2023-08-15 14:17:15 +0200

    Integration is done

--- src/DataSci4Chem.jl
+++ src/DataSci4Chem.jl
@@ -18,7 +18,7 @@ include("Added_Functions.jl")
 
 export notebook, plot, plot!, xlabel!, ylabel!, jupyterlab, scatter, scatter!, bar, bar!, xlims!, ylims!, sp, dataset, describe, histogram,
  histogram!, scatter3d, scatter3d!, heatmap, heatmap!, savefig, twinx, transpose, pinv, inv, newton_root, bisection_root, numerical_derivative, 
- find_zero, find_zeros, mean, std, var, midpoint_integration
+ find_zero, find_zeros, mean, std, var, midpoint_integration, trapezoid_rule, simpsons_rule
 
 
 # Write your package code here.
