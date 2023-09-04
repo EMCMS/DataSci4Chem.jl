803b28956f194f03bbfd2eeab745f8b69637bac1
Saer Samanipour <saersamani@gmail.com>
2023-09-04 21:57:16 +0200

    Last updates

--- src/DataSci4Chem.jl
+++ src/DataSci4Chem.jl
@@ -20,7 +20,7 @@ include("Added_Functions.jl")
 export notebook, plot, plot!, xlabel!, ylabel!, jupyterlab, scatter, scatter!, bar, bar!, xlims!, ylims!, sp, dataset, describe, histogram,
  histogram!, scatter3d, scatter3d!, heatmap, heatmap!, savefig, twinx, transpose, pinv, inv, newton_root, bisection_root, numerical_derivative, 
  find_zero, find_zeros, mean, std, var, midpoint_integration, trapezoid_rule, simpsons_rule, curve_fit, estimate_covar, standard_error, 
- confidence_interval, median, DataFrame, replace!, corspearman, cor  
+ confidence_interval, median, DataFrame, replace!, corspearman, cor, svd  
 
 
 # Write your package code here.
